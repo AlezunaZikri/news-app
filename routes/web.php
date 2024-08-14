45d03a8cc7d3afde00d6fc0179538a78fc379b2f
alezunazikri <pohanzikri2@gmail.com>
2024-08-14 09:09:28 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class,'index']);
 
 Route::resource('post', App\Http\Controllers\PostController::class);
